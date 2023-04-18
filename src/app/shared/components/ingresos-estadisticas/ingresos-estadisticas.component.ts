fa1af387eac12ec71fbbce0925cb1e7b8909b7ba
José <60800464+fjose-wd@users.noreply.github.com>
2023-04-18 15:10:19 -0500

    integracion de vistas con la api para la opcion ingresos-estadisticas

--- src/app/shared/components/ingresos-estadisticas/ingresos-estadisticas.component.ts
+++ src/app/shared/components/ingresos-estadisticas/ingresos-estadisticas.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NgBlockUI, BlockUI } from 'ng-block-ui';
+import { PerfectScrollbarDirective, PerfectScrollbarComponent, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import Swal from 'sweetalert2';
+import {AttentionConsultation} from '../../../interfaces/attentionConsultation';
+import {ApiResponse} from '../../../interfaces/response';
+import * as moment from 'moment';
+import { Page } from '../../../models/forms-data/page';
+import { ColumnMode, NgxDatatableModule, DatatableComponent  } from '@swimlane/ngx-datatable';
+import * as XLSX from 'xlsx';
+import { ExcelJson } from '../../../interfaces/excel-json.interface';
+import { ExportService } from '../../../_services/export.service';
+import ResizeObserver from 'resize-observer-polyfill';
+import { CurrencyPipe } from '@angular/common';
+import { CustomNumberPipe } from 'src/app/pipes/customNumber.pipe';
+import { PhonePipe } from 'src/app/pipes/phone.pipe';
+import { TesoreriaService } from 'src/app/_services/tesoreria.service';
+import { NumberDecimalPipe } from 'src/app/pipes/numberDecimal.pipe';
 
 @Component({
   selector: 'app-ingresos-estadisticas',
@@ -6,10 +24,309 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresos-estadisticas.component.scss']
 })
 export class IngresosEstadisticasComponent implements OnInit {
+  initialSize = 0;
+  active = 1;
+  enableSummary = true;
+  summaryPosition = 'bottom';
+  filtroForm: FormGroup;
+  @BlockUI('addRows') blockUIAddRows: NgBlockUI;
+  @BlockUI('rowSelection') blockUIRowSelection: NgBlockUI;
 
-  constructor() { }
+  public config: PerfectScrollbarConfigInterface = { wheelPropagation: true };
+  @ViewChild(PerfectScrollbarComponent) componentRef?: PerfectScrollbarComponent;
+  @ViewChild(PerfectScrollbarDirective, { static: true }) directiveRef?: PerfectScrollbarDirective;
 
-  ngOnInit(): void {
+  @ViewChild(DatatableComponent) private table: DatatableComponent;
+  options = {
+    close: true,
+    expand: true,
+    minimize: true,
+    reload: true
+  };
+  temp = [];
+  selected = [];
+  id: number;
+  loadingIndicator: true;
+  rows: any;
+  rows1 = [];
+  rows2 = [];
+  rows3 = [];
+  rows4 = [];
+  editing = {};
+  row: any;
+  public breadcrumb: any;
+  data:any;
+  parameters:any;
+  message;
+  title;
+  columns:any;
+  columns1:any;
+  columns2:any;
+  columns3:any;
+  columns4:any;
+  optionsWithCaption = {};
+  datePipe: any;
+        // f_inicio: '2022-11-01',
+      // f_fin: '2022-11-30',
+  fecha = moment(new Date()).format('YYYY-MM-DD');
+  sede = '0001';
+  estado = 'T';
+  tipo_lista = 'E';
+
+  page = new Page()
+  ColumnMode = ColumnMode;
+  filtered;
+  public pageLimitOptions = [
+    {value: 10},
+    {value: 25},
+    {value: 50},
+    {value: 100},
+  ];
+  totalMorosos:any;
+  totalAfiliados:any;
+  totalPeriodos:any;
+  totalDeuda:any;
+  optionsMes = [
+    { value: '01', label: 'Enero' },
+    { value: '02', label: 'Febrero' },
+    { value: '03', label: 'Marzo' },
+    { value: '04', label: 'Abril' },
+    { value: '05', label: 'Mayo' },
+    { value: '06', label: 'Junio' },
+    { value: '07', label: 'Julio' },
+    { value: '08', label: 'Agosto' },
+    { value: '09', label: 'Setiembre' },
+    { value: '10', label: 'Octubre' },
+    { value: '11', label: 'Noviembre' },
+    { value: '12', label: 'Diciembre' },
+  ];
+  optionsAnio = [];
+  mes = moment(new Date()).format('MM');
+  anio = moment(new Date()).format('YYYY');
+  periodo = this.anio + this.mes;
+  constructor(private tableApiservice: TesoreriaService, private exportService: ExportService, private _cnp:CustomNumberPipe,
+    private _cp: CurrencyPipe, private _phone: PhonePipe, private _ndp:NumberDecimalPipe) {
+    this.page.pageNumber = 0;
+    this.page.size = 10;
+
+    this.filtroForm = new FormGroup({
+      anio: new FormControl(this.anio),
+      mes: new FormControl(this.mes),
+
+  });
+  var anioOp = Number(this.anio);
+  while ( Number(anioOp) > 2017 ) {
+    console.log(275, anioOp);
+    
+    const anioNew = {
+       value: anioOp.toString(), label: anioOp.toString() 
+    }
+    this.optionsAnio.push(anioNew);
+    anioOp--;
+  }
+   }
+
+  ngOnInit() {
+  
+    // this.setPage({ offset: 0 });
+  }
+  getRowClass(row) {
+
+    // return {
+    //   'totals': row.Programa.includes('TOTAL')
+    // };
+  }
+  getRowClass1(row) {
+
+    // return {
+    //   'totals': row.periodo.includes('TOTAL')
+    // };
+  }
+  public onLimitChange(limit: any): void {
+    this.changePageLimit(limit);
+    // this.setPage({ offset: 0 });
+
+  }
+  // public onAnioChange(anio: any): void {
+  //   this.anio = anio;
+  //   this.periodo = this.anio + this.mes;
+  //   // this.setPage({ offset: 0 });
+  // }
+  // public onMesChange(mes: any): void {
+  //   this.mes = mes;
+  //   this.periodo = this.anio + this.mes;
+  //   // this.setPage({ offset: 0 });
+  // }
+  private changePageLimit(limit: any): void {
+    this.loading();
+    if (limit === '0'){
+      
+      this.page.size = this.page.totalElements;
+      console.log(this.page.totalElements);
+      return
+    }
+    this.page.size = parseInt(limit, 10);
+    setTimeout(() => {
+      Swal.close();
+    }, 1000)
+  }
+  setPage(pageInfo) {
+    console.log(pageInfo);
+    this.page.pageNumber = pageInfo.offset;
+    this.parameters = {
+      anio: this.anio,
+      mes: this.mes,
+      periodo:this.periodo,
+      pageNumber: this.page.pageNumber,
+      size: this.page.size
+    };
+
+    this.loading();
+    this.tableApiservice.IngGetIngresosResumen(this.parameters).subscribe(
+      (response) => {
+        this.rows = [];
+        console.log(response);
+        if(response.data.success){
+          this.data = response.data ? response.data : [];
+          this.message = this.data.titulo;
+          this.title = response.data.title;
+          // // //console.log(response.data);
+          
+          // // //console.log(168, this.data);   
+          this.columns = this.data.cabeceras_ingresos_TPac;
+          this.rows = this.data.tabla_ingresos_TPac;
+          // this.temp = this.rows;
+          this.columns1 = this.data.cabeceras_ingresos_empresa;
+          this.rows1 = this.data.tabla_ingresos_empresa;
+          this.columns2 = this.data.cabeceras_ingresos_sede_anual;
+          this.rows2 = this.data.tabla_ingresos_sede_anual;
+
+          this.columns3 = this.data.cabeceras_ingresos_tPaciente_anual;
+          this.rows3 = this.data.tabla_ingresos_tPaciente_anual;
+          this.columns4 = this.data.cabeceras_ingresos_empresa_anual;
+          this.rows4 = this.data.tabla_ingresos_empresa_anual;
+          // console.log(169, this.rows1);
+
+            Swal.close();
+        }else{
+          Swal.close();
+        }
+        
+      },
+      (error) => {
+          Swal.close();
+      }
+    );
   }
 
+  copyTableToClipboard(numberTabla){
+    if(numberTabla === 0){
+      // this.rows.map(item=>{
+      //   item.ImpCuotasVencidas = this._cp.transform( item.ImpCuotasVencidas);
+      // });
+      this.exportService.exportToClipboard(this.rows, this.columns);
+    }else if (numberTabla === 1){
+
+      this.exportService.exportToClipboard(this.rows2, this.columns2);
+    }else if (numberTabla === 2){
+    
+      this.exportService.exportToClipboard(this.rows2, this.columns2);
+    }
+  }
+
+  exportToExcel(numberTabla): void {
+    if(numberTabla === 0){
+      // this.rows.map(item=>{
+      //   item.ImpCuotasVencidas = this._cp.transform( item.ImpCuotasVencidas);
+      // });
+      this.exportService.exportTableElmToExcel(this.rows, 'Listado de Morosos');
+    }else if (numberTabla === 1){
+      
+      this.exportService.exportTableElmToExcel(this.rows1, 'Listado de Morosos - Distribución por Programa');
+    }else if (numberTabla === 2){
+      
+      this.exportService.exportTableElmToExcel(this.rows2, 'Listado de Morosos - Distribución por Período');
+    }
+  }
+
+  filter() {
+  
+        const form = this.filtroForm.value;
+          this.anio = form.anio;
+          this.mes = form.mes;
+
+        this.setPage({ offset: 0 });
+    }
+  async loading() {
+      Swal.fire({
+          html: "<div>Filtrando ...</div>",
+          width: "200px",
+          allowEscapeKey: false,
+          allowOutsideClick: false,
+          onOpen: () => {
+              Swal.showLoading();
+          },
+      });
+  }
+  updateFilter(event) {
+    const input = event.target.value.toLowerCase();
+
+    // filter our data
+    if (input.length > 0) {
+      const filtered = this.rows.filter(el =>
+        // console.log(240, el);
+          Object.values(el).find( val => val?.toString().toLowerCase().includes(input) ) != undefined
+        );
+        console.log(filtered);
+      this.rows = [...filtered]
+      
+    } else {
+      console.log(this.filtered);
+      this.rows = [...this.temp]
+    }
+  }
+  onSelect({ selected }) {
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+  summaryForAmount(cells: any){
+    // console.log(cells);
+    let count: number = 0;
+    let re = /\,/gi;
+    let re1 = /\S\/./gi;
+    let re2 = /\S\//gi;
+        
+        cells.filter((cell) => {
+            cell = cell.toString();
+            if (cell != null && cell != undefined) {
+              
+              if (cell.indexOf('S/') > -1){  
+                count = count + +cell.replace(re2, '').replace(',', '');
+              }else if (!(cell.indexOf('-') > -1 || cell.indexOf('(') > -1)) {
+              //  console.log(719, typeof cell, count)
+                    count = count + +cell.replace(re, '');
+                    // console.log(722,cell, count)
+              }else if (cell.indexOf('-') > -1) {
+                    count = count + 0;
+              }else if (cell.indexOf('(') > -1) {
+                let number = cell.replace('(', '').replace(')', '');
+                count = count - +number.replace(re, '');
+              }
+            }
+        });
+        
+        if(!count){
+          return count.toString().replace('NaN', 'Total');
+        }else if (count.toString().indexOf('.') > -1){
+
+          return count.toString().indexOf('-') > -1 ? count.toLocaleString().replace('-', '(').concat(')') : 'S/ ' + count.toLocaleString();
+        }else{
+          return count.toString().indexOf('-') > -1 ? count.toLocaleString().replace('-', '(').concat(')') : count.toLocaleString();
+        }
+  }
+  private summaryNull(cells: any): string {
+    // console.log(739, cells)
+        return 'TOTAL';
+  }
 }
+
