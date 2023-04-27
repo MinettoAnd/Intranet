4a2963069456c615cc4637a7ef7c8c7607e3ad15
CMSWebMaster <webmaster@clubdelasalud.pe>
2023-04-27 09:24:44 -0500

    Icono Genero Personal

--- utils/GenderCellRenderer.ts
+++ utils/GenderCellRenderer.ts
@@ -13,7 +13,7 @@ export class GenderCellRenderer implements INoRowsOverlayAngularComp {
     const image =
       params.value === 'MA' ? 'user_masculino.png' : 'user_femenino.png';
     //this.imageSource = `./assets/img/${image}`;
-    this.imageSource = `http://webserver.maisondesante.org.pe/html2/sgi/recursos/css/images/icons16/${image}`;
+    this.imageSource = `./assets/images/${image}`;
     this.value = params.value;
   }
 }
