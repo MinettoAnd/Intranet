a9284fd7e30038bc830f976d03324ac78664591e
José <60800464+fjose-wd@users.noreply.github.com>
2023-05-12 16:02:34 -0500

    se termino la opcion facturacion/reporteExpedientes

--- src/app/pages/facturacion/reporte-expedientes/reporte-expedientes.component.ts
+++ src/app/pages/facturacion/reporte-expedientes/reporte-expedientes.component.ts
@@ -1,4 +1,4 @@
-import { map } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 import { Component, ElementRef, OnInit, PipeTransform, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { NgBlockUI, BlockUI } from 'ng-block-ui';
@@ -14,19 +14,27 @@ import * as XLSX from 'xlsx';
 import { ExcelJson } from '../../../interfaces/excel-json.interface';
 import { ExportService } from '../../../_services/export.service';
 import { CurrencyPipe } from '@angular/common';
-
+import { Observable, of } from 'rxjs';
 import ResizeObserver from 'resize-observer-polyfill';
-import {FirstDataRenderedEvent, GridApi, GridOptions, GridReadyEvent} from "ag-grid-community";
+import {FirstDataRenderedEvent, GridApi, GridOptions, GridReadyEvent, PaginationChangedEvent} from "ag-grid-community";
 import { AgGridAngular } from "ag-grid-angular";
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { RowDetalleComponent } from 'src/app/modals/jpric/row-detalle/row-detalle.component';
 import { LinkRendererComponent } from 'src/app/shared/components/renderer/link-renderer.component';
+import { PagedData } from '../../../models/forms-data/paged-data';
 
+interface PageInfo {
+  offset: number;
+  pageSize: number;
+  limit: number;
+  count: number;
+}
 @Component({
   selector: 'app-reporte-expedientes',
   templateUrl: './reporte-expedientes.component.html',
   styleUrls: ['./reporte-expedientes.component.scss']
 })
+
 export class ReporteExpedientesComponent implements OnInit {
   active = 1;
   closeResult = '';
@@ -48,6 +56,7 @@ export class ReporteExpedientesComponent implements OnInit {
   ];
   optionsAnio = [];
   private isLoading: boolean = true;
+  action: boolean = false;
   private baseChart: ElementRef;
   // private baseChart2: ElementRef;
   color = [ 'graph-primary', 'primary','graph-tertiary', 'graph-quaternary '];
@@ -116,6 +125,7 @@ export class ReporteExpedientesComponent implements OnInit {
     montoTotal: '',
   };
   private rowClassRules;
+  private aggfunc;
   columns1: any;
   rows1: any[];
   rows1filtered: any;
@@ -167,7 +177,7 @@ export class ReporteExpedientesComponent implements OnInit {
   medicos = false;
   rowsModal: any[];
   columnsModal: any[];
-  rowsMedicoRecord: any[];
+  rowsTemp: any[];
   filtered: any;
   detalleAnual: any;
   especialidad: any;
@@ -200,9 +210,18 @@ export class ReporteExpedientesComponent implements OnInit {
   data:any;
   message;
   title;
-  private gridApi!: GridApi;
+  isLoad = 0;
+  private gridApi1!: GridApi;
+  private gridApi2!: GridApi;
   private gridColumnApi;
   public paginationPageSize = 10;
+  totalElements1: number;
+  pageNumber1: number;
+  totalElements2: number;
+  pageNumber2: number;
+  rowsT1: any[];
+  rowsT2: any[];
+  
   constructor(private tableApiservice: FacturacionService, private exportService: ExportService,
     private _cp: CurrencyPipe, private modalService: NgbModal) { 
       this.page1.pageNumber = 0;
@@ -213,6 +232,7 @@ export class ReporteExpedientesComponent implements OnInit {
       this.page3.size = 10;
       this.page4.pageNumber = 0;
       this.page4.size = 10;
+      this.pageNumber1 = 0
     this.filtroForm = new FormGroup({
       id_sede: new FormControl(this.id_sede),
       mes: new FormControl(this.mes),
@@ -232,23 +252,62 @@ export class ReporteExpedientesComponent implements OnInit {
     }
     this.rowClassRules = {
       "totals": function(params) {
-         console.log(301, params); 
+        //  console.log(301, params); 
         var totales;
         if(params.data.aseguradora_Nombre !== undefined){
           totales = params.data.aseguradora_Nombre;
-        }else if(params.data.grupo !== undefined){
-          totales = params.data.grupo;
+        }else if(params.data.aseguradoraNombre !== undefined){
+          totales = params.data.aseguradoraNombre;
         }
         return totales === 'TOTAL';
       },
     };
+    // this.aggfunc = (params: any) => {
+    //   console.log(248, params)
+    //   var amtTotal = 0; 
+    //   params.values.forEach((value: { totalAmt: number }) =>
+    //   { 
+    //     if (value && value.totalAmt) {
+    //      amtTotal += value.totalAmt;
+    //   }
+    //   });
+    //   var total = (amtTotal / 100000);
+    //   return {
+    //   totalAmt: amtTotal,
+    //   toString: () => { return (amtTotal) ? (total).toFixed(2) : '' }
+      
+    //   } 
+    // }
+  //   const calcTotalCols = ['monto_total'];
+  //   const totalRow = function(api) {
+  //     let result = [{}];
+  //     // initialize all total columns to zero
+  //     calcTotalCols.forEach(function (params){
+  //         result[0][params] = 0
+  //     });
+  //    // calculate all total columns
+  //     calcTotalCols.forEach(function (params){
+  //         this.rows16.forEach(function (line) {
+  //             result[0][params] += line[params];
+  //         });
+  //     });
+  //     api.setPinnedBottomRowData(result);
+  // }
+  // const gridOptions = {
+  //   columnDefs: this.columns16,
+  //   rowData: this.rows16
+  // };
     this.frameworkComponents = {
       buttonRenderer: LinkRendererComponent,
     }
     this.changeTable = false;
   }
+  ngOnInit(){
+
+    // this.setPage({ offset: 0 });
+  }
   CurrencyCellRendererPEN(params: any) {
-    console.log(251, params)
+    // console.log(251, params)
     var inrFormat = new Intl.NumberFormat('es-PE', {
       style: 'currency',
       currency: 'PEN',
@@ -256,13 +315,208 @@ export class ReporteExpedientesComponent implements OnInit {
     });
     return inrFormat.format(params.value);
   }
+async setPage1(pageInfo: PageInfo) {
+    this.pageNumber1 = pageInfo.offset;
+    const rowOffset = pageInfo.offset * pageInfo.pageSize;
+  
+    this.page1.pageNumber = Math.floor(rowOffset / this.page1.size);
+    this.isLoad++;
+    console.log(323, this.page1, rowOffset , this.page1.size);
+    // if (this.page1.pageNumber === 'NaN'){
 
-  ngOnInit(){
+    // }
+   await this.getResults(this.page1, this.rows11).subscribe(  pagedData => {
+      console.log(1187, this.page1, this.rows11);
+      // this.rowsT1 = [];
+      this.totalElements1 = pagedData.page.totalElements;
+      if (pagedData.data.length < pagedData.page.size){
+        this.rowsT1 = pagedData.data
+        if(pagedData.data.length > 0){
+          this.totalRow1(this.gridApi1, this.rowsT1);
+        }
+        // for (let index = pagedData.data.length; index < pagedData.page.size; index++) {
+        //   this.rowsT1[(index)] = { id_esp: '', especialidad: ''};
+        // }
+      }else{
+        this.rowsT1 = pagedData.data;
+        if(this.gridApi1){
+          this.totalRow1(this.gridApi1, this.rowsT1);
+        }
+      }
+      this.isLoad--;
+    });
+  }
+  onPaginationChanged(table) {
+    if (table === 1){
+      // console.log(336, this.pageNumber1 )
+      if(this.gridApi1) {
+         
+        if (this.pageNumber1 === this.gridApi1.paginationGetCurrentPage()) {
+          
+          return;
+        }else{
+          
+          this.setPage1({
+            offset: this.gridApi1.paginationGetCurrentPage(),
+            pageSize: this.gridApi1.paginationGetPageSize(),
+            limit: 10,
+            count: 0
+          });
+          
+          
+        }
+        this.pageNumber1 = this.gridApi1.paginationGetCurrentPage()
+      }else{
+        return;
+      }
+    }else if (table === 2){
+      if(this.gridApi2) {
+        //  console.log(336, this.pageNumber1 , this.gridApi.paginationGetCurrentPage())
+        if (this.pageNumber2 === this.gridApi2.paginationGetCurrentPage()) {
+          
+          return;
+        }else{
+          
+          this.setPage2({
+            offset: this.gridApi2.paginationGetCurrentPage(),
+            pageSize: this.gridApi2.paginationGetPageSize(),
+            limit: 10,
+            count: 0
+          });
+          
+          
+        }
+        this.pageNumber2 = this.gridApi2.paginationGetCurrentPage()
+      }else{
+        return;
+      }
+    }
+    
 
-    // this.setPage({ offset: 0 });
+  }
+  async setPage2(pageInfo: PageInfo) {
+    console.log('object')
+    
+    this.pageNumber2 = pageInfo.offset;
+    const rowOffset = pageInfo.offset * pageInfo.pageSize;
+    this.page2.pageNumber = Math.floor(rowOffset / this.page2.size);
+    this.isLoad++;
+    console.log(323, this.page2, rowOffset , this.page2.size);
+    await this.getResults(this.page2, this.rows16).subscribe(pagedData => {
+      console.log(1188, this.page2, this.rows16);
+      // this.rowsT2 = [];
+      this.totalElements2 = pagedData.page.totalElements;
+      if (pagedData.data.length < pagedData.page.size){
+        this.rowsT2 = pagedData.data
+        console.log(413, this.rowsT2)
+        if(pagedData.data.length > 0){
+        this.totalRow2(this.gridApi2, this.rowsT2);
+        }
+        // for (let index = pagedData.data.length; index < pagedData.page.size; index++) {
+        //   this.rowsT2[(index)] = { CIE10: '', Diagnostico: '' };
+        // }
+      }else{
+        this.rowsT2 = pagedData.data;
+        console.log(415, this.rowsT2)
+        if(this.gridApi2){
+          this.totalRow2(this.gridApi2, this.rowsT2);
+        }
+        
+      }
+      this.isLoad--;
+    });
+  }
+  public getResults(page: Page, data: any[]) {
+    return of(data)
+      .pipe(map(d => this.getPagedData(page, data)))
+      .pipe(delay(1000 * Math.random()));
+  }
+  
+  private getPagedData(page: Page, data: any[]) {
+    // console.log(1151, page, data);
+    const pagedData = new PagedData();
+    page.totalElements = data.length;
+    page.totalPages = page.totalElements / page.size;
+    const start = page.pageNumber * page.size;
+    const end = Math.min(start + page.size, page.totalElements);
+    // console.log(1157, start, end);
+    for (let i = start; i < end; i++) {
+      const jsonObj = data[i];
+      pagedData.data.push(jsonObj);
+    }
+    pagedData.page = page;
+    console.log(1163, pagedData);
+    return pagedData;
+  }
+  onGridReady1(params: GridReadyEvent) {
+    this.gridApi1 = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.totalRow1(this.gridApi1, this.rowsT1);
+    console.log(445, this.rowsT1);
+  }
+  onGridReady2(params: GridReadyEvent) {
+    this.gridApi2 = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.totalRow2(this.gridApi2, this.rowsT2);
+    console.log(451, this.rowsT2);
+  }
+
+totalRow1(gridApi: GridApi, rows) {
+    let result = [{
+      aseguradoraNombre: 'TOTAL',
+      conteo_lima: 0,
+      monto_lima:0 ,
+      conteo_chorrillos: 0,
+      monto_chorrillos: 0,
+      conteo_surco: 0,
+      monto_surco: 0,
+      monto_total: 0,
+      conteo_total: 0
+    }];
+ console.log(572, rows)
+        rows.forEach(function (line) {
+          result[0].conteo_lima += Number(line.conteo_lima);
+          result[0].monto_lima += Number(line.monto_lima);
+          result[0].conteo_chorrillos += Number(line.conteo_chorrillos);
+          result[0].conteo_surco += Number(line.conteo_surco);
+          result[0].monto_surco += Number(line.monto_surco);
+          result[0].monto_total += Number(line.monto_total);
+          result[0].conteo_total += Number(line.conteo_total);
+        });
+  //   });
+  // console.log(572, result)
+  // gridApi.setPinnedBottomRowData(this.rows16)
+    gridApi.setPinnedBottomRowData(result as any);
+  }
+  totalRow2(gridApi: GridApi, rows) {
+    let result = [{
+      aseguradoraNombre: 'TOTAL',
+      conteo_lima: 0,
+      monto_lima:0 ,
+      conteo_chorrillos: 0,
+      monto_chorrillos: 0,
+      conteo_surco: 0,
+      monto_surco: 0,
+      monto_total: 0,
+      conteo_total: 0
+    }];
+ console.log(572, rows)
+        rows.forEach(function (line) {
+          result[0].conteo_lima += Number(line.conteo_lima);
+          result[0].monto_lima += Number(line.monto_lima);
+          result[0].conteo_chorrillos += Number(line.conteo_chorrillos);
+          result[0].conteo_surco += Number(line.conteo_surco);
+          result[0].monto_surco += Number(line.monto_surco);
+          result[0].monto_total += Number(line.monto_total);
+          result[0].conteo_total += Number(line.conteo_total);
+        });
+  //   });
+  // console.log(572, result)
+  // gridApi.setPinnedBottomRowData(this.rows16)
+    gridApi.setPinnedBottomRowData(result as any);
   }
   filter() {
-
+    this.action = true
     const form = this.filtroForm.value;
       this.id_sede = form.id_sede,
       this.mes = form.mes,
@@ -438,6 +692,12 @@ export class ReporteExpedientesComponent implements OnInit {
                 }
               })
               this.rows11 = this.data.tabla_expedientes_facturado_emp
+              this.setPage1({
+                offset: 0,
+                pageSize: 0,
+                limit: 10,
+                count: 0
+              });
               this.temp1 = this.rows11;
 
               this.columns12 = this.data.cabeceras_expedientes_pendientes_soles
@@ -460,6 +720,12 @@ export class ReporteExpedientesComponent implements OnInit {
               //   }
               // })
               this.rows16 = this.data.tabla_expedientes_pendientes_aseguradora
+              this.setPage2({
+                offset: 0,
+                pageSize: 0,
+                limit: 10,
+                count: 0
+              });
               this.columns16.map(item => {
                 // console.log(301, item)
                 if(item.children){
@@ -471,14 +737,14 @@ export class ReporteExpedientesComponent implements OnInit {
                       field: subitem.field
                     }
                     // if(subitem.aggFunc === 'sum'){
-                      // subitem.aggFunc = 'sum'
-                      // subitem.aggFunc = 
-                      // aggFunc(params: any) => {
+                      // subitem.aggFunc = this.aggfunc
+                      // subitem.aggFunc = (params: any) => {
+                      //   console.log(476, params)
                       //   var amtTotal = 0; 
                       //   params.values.forEach((value: { totalAmt: number }) =>
-                      //   { if (value && value.totalAmt) {
-                      //   amtTotal += value.totalAmt;
-                        
+                      //   { 
+                      //     if (value && value.totalAmt) {
+                      //      amtTotal += value.totalAmt;
                       //   }
                       //   });
                       //   var total = (amtTotal / 100000);
@@ -516,11 +782,6 @@ export class ReporteExpedientesComponent implements OnInit {
         );
 
   }
-  onGridReady(params: GridReadyEvent) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-    console.log(499, this.gridColumnApi);
-  }
 
   // onPageSizeChanged() {
   //   var value = (document.getElementById('page-size') as HTMLInputElement)
@@ -532,7 +793,7 @@ export class ReporteExpedientesComponent implements OnInit {
   public onLimitChange(limit: any, numberT): void {
     this.changePageLimit(limit, numberT);
     // this.setPage({ offset: 0 });
-
+    
   }
   
 
@@ -545,7 +806,13 @@ export class ReporteExpedientesComponent implements OnInit {
         return
       }
       this.page1.size = parseInt(limit, 10);
-      this.gridApi.paginationSetPageSize(Number(this.page1.size));
+      this.setPage1({
+        offset: this.gridApi1.paginationGetCurrentPage(),
+        pageSize: this.gridApi1.paginationGetPageSize(),
+        limit: 10,
+        count: 0
+      });
+      this.gridApi1.paginationSetPageSize(Number(this.page1.size));
       console.log(this.page1.size);
     }else if(numberT === '2'){
       if (limit === '0'){
@@ -554,7 +821,13 @@ export class ReporteExpedientesComponent implements OnInit {
         return
       }
       this.page2.size = parseInt(limit, 10);
-      this.gridApi.paginationSetPageSize(Number(this.page2.size));
+      this.setPage2({
+        offset: this.gridApi2.paginationGetCurrentPage(),
+        pageSize: this.gridApi2.paginationGetPageSize(),
+        limit: 10,
+        count: 0
+      });
+      this.gridApi2.paginationSetPageSize(Number(this.page2.size));
     }if(numberT === '3'){
       if (limit === '0'){
         this.page3.size = this.page3.totalElements;
@@ -936,4 +1209,5 @@ export class ReporteExpedientesComponent implements OnInit {
       return 'TOTAL';
     }
 }
+
 
