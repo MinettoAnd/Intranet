3e3d62d86839c196d8ddea13d8c53356295369df
code-sys <emersonhans2@gmail.com>
2023-09-01 18:37:49 -0500

    Avance formato y footer de totales

---
+++ src/app/shared/components-shared/reporte/enums/marcador.enum.ts
@@ -0,0 +1,5 @@
+export enum MarcadorEnum {
+  /* Tablas */
+  // Totales
+  IsTotal = 'istotal',
+}
---
+++ src/app/shared/components-shared/reporte/helpers/obt-totales.helper.ts
@@ -0,0 +1,30 @@
+import { fnDestArrObj } from "src/app/shared/utils/dest-arr-obj.util";
+import { fnSumFilasTbl } from "./sum-filas-tbl.helper";
+import { MarcadorEnum } from "../enums/marcador.enum";
+
+export function fnObtTotales(
+  datos: Array<any>,
+  excluir: Array<string> = [],
+): {
+  totales: Array<any>,
+  calculados: boolean,
+} {
+  let calculados: boolean = false;
+  let totales = fnDestArrObj(
+    datos.filter(
+      obj => obj['grupo'].trim().toLowerCase() == 'total'
+    )
+  );
+
+  if (totales.length == 0) {
+    excluir.push('grupo');
+    totales = [fnSumFilasTbl(datos, excluir)];
+    calculados = true;
+  }
+
+  for (let total of totales) {
+    total[MarcadorEnum.IsTotal] = true;
+  }
+
+  return { totales, calculados };
+}
